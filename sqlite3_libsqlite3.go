9fea8830415123f9b6ad718257c80770a9228c6d
litesync <contact@litesync.io>
2021-04-08 22:18:35 -0300

    link to the pre-installed LiteSync library

--- sqlite3_libsqlite3.go
+++ sqlite3_libsqlite3.go
@@ -9,11 +9,11 @@ package sqlite3
 
 /*
 #cgo CFLAGS: -DUSE_LIBSQLITE3
-#cgo linux LDFLAGS: -lsqlite3
-#cgo darwin LDFLAGS: -L/usr/local/opt/sqlite/lib -lsqlite3
-#cgo darwin CFLAGS: -I/usr/local/opt/sqlite/include
-#cgo openbsd LDFLAGS: -lsqlite3
-#cgo solaris LDFLAGS: -lsqlite3
-#cgo windows LDFLAGS: -lsqlite3
+#cgo linux LDFLAGS: -llitesync
+#cgo darwin LDFLAGS: -L/usr/local/lib -llitesync
+#cgo darwin CFLAGS: -I/usr/local/include
+#cgo openbsd LDFLAGS: -llitesync
+#cgo solaris LDFLAGS: -llitesync
+#cgo windows LDFLAGS: -llitesync
 */
 import "C"
